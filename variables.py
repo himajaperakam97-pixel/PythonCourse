54c9b59128778a94862d82de0782adebd7228bec
perakam himaja <perakamhimaja97@gmail.com>
2026-09-03 15:04:21 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,136 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#variables
+print(5+9)
+14
+a=20
+b=30
+print(a+b)
+50
+d=50
+print(d)
+50
+E=100
+print(E)
+100
+e=100
+print(E)
+100
+e=100
+print(E)
+100
+h=200
+print(H)
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    print(H)
+NameError: name 'H' is not defined. Did you mean: 'h'?
+name="himaja"
+print(name)
+himaja
+print("name")
+name
+city="vijayawada"
+print(city)
+vijayawada
+country "india"
+SyntaxError: invalid syntax
+country="india"
+print(country)
+india
+3=8
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a3=8
+print(a3)
+8
+8a=90
+SyntaxError: invalid decimal literal
+a0123456789=100
+print(a0123456789)
+100
+firstname="himaja"
+a=4,b=9
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=2;b=9
+print(a+b)
+11
+a,b=3,4
+print(a+b)
+7
+_a=40
+print(_a)
+40
+$=9
+SyntaxError: invalid syntax
+b=5
+ b=5
+ 
+SyntaxError: unexpected indent
+a,b,c
+Traceback (most recent call last):
+  File "<pyshell#41>", line 1, in <module>
+    a,b,c
+NameError: name 'c' is not defined
+a=2,3,4,5,6,7
+print(a)
+(2, 3, 4, 5, 6, 7)
+a,b,c=5,6,7
+print(a,b,c)
+5 6 7
+a,b,c=4,5,6,7,8,9
+Traceback (most recent call last):
+  File "<pyshell#46>", line 1, in <module>
+    a,b,c=4,5,6,7,8,9
+ValueError: too many values to unpack (expected 3, got 6)
+a,b,c=(4,5,6)
+print(a,b,c)
+4 5 6
+#this is called unpacking
+first name="himaja"
+SyntaxError: invalid syntax
+first_name="himaja"
+print(first_name)
+himaja
+firstname="pooja"
+print(first_name)
+himaja
+fname="himaja"
+lname="perakam"
+print(fname+lname)
+himajaperakam
+print(fname+" "+lname)
+himaja perakam
+a=7
+print(a)
+7
+del a
+print(a)
+Traceback (most recent call last):
+  File "<pyshell#62>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a3'?
+>>> name="himaja"
+>>> print(name)
+himaja
+>>> NAME="himaja"
+>>> print(NAME)
+himaja
+>>> Name="himaja"
+>>> print(Name)
+himaja
+>>> a=10
+>>> print(a)
+10
+>>> b=5
+>>> print(a)
+10
+>>> print(c)
+6
+>>> print(d)
+50
+>>> if=10
+SyntaxError: invalid syntax
+>>> while=90
+SyntaxError: invalid syntax
+>>> del=70
+SyntaxError: invalid syntax
